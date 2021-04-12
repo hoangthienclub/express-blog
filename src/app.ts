8c4064cb898c0c994d5e1d1c75420085564c4b4f
Tran Thien <thien.tran@saigonvalley.com>
2021-04-12 14:48:55 +0700

    init and Post

---
+++ src/app.ts
@@ -0,0 +1,42 @@
+import * as express from 'express';
+import * as bodyParser from 'body-parser';
+import * as mongoose from 'mongoose';
+import Controller from './interface/controller.interface';
+
+class App {
+  public app: express.Application;
+  constructor(controllers: Controller[]) {
+    this.app = express();
+
+    this.connectToTheDatabase();
+    this.initializeMiddlewares();
+    this.initializeControllers(controllers);
+  }
+
+  private initializeMiddlewares() {
+    this.app.use(bodyParser.json());
+  }
+
+  private initializeControllers(controllers) {
+    controllers.forEach((controller) => {
+      this.app.use('/', controller.router)
+    });
+  }
+
+  private connectToTheDatabase() {
+    const {
+      MONGO_USER,
+      MONGO_PASSWORD,
+      MONGO_PATH,
+    } = process.env;
+    mongoose.connect(`mongodb://${MONGO_PATH}/blog`, { useNewUrlParser: true, useUnifiedTopology: true });
+  }
+
+  public listen() {
+    this.app.listen(process.env.PORT, () => {
+      console.log(`App listening on the port ${process.env.PORT}`);
+    });
+  }
+}
+
+export default App;
---
+++ src/server.ts
@@ -0,0 +1,15 @@
+
+import 'dotenv/config';
+import validateEnv from './utils/validateEnv';
+import App from './app';
+import PostsController from './posts/posts.controller';
+
+validateEnv();
+
+const app = new App(
+  [
+    new PostsController()
+  ]
+)
+
+app.listen();
